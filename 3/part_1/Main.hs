5d5a4b2a6bf2b797de18fbd74736f4099842b405
Daniel Liptak <liptakdani93@gmail.com>
2025-01-03 23:00:06 +0100

    Implement multiplication extraction and calculation from input file (Part 1)

--- 3/part_1/Main.hs
+++ 3/part_1/Main.hs
@@ -1,4 +1,10 @@
 module Main where
 
+import Part1
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    content <- readFile "input.txt"
+    let mulStrings = getMuls content
+    let result = sum $ map getLineMul mulStrings
+    print result
--- 3/part_1/tests/Part1Test.hs
+++ 3/part_1/tests/Part1Test.hs
@@ -3,13 +3,38 @@ module Main where
 import Part1
 import Test.HUnit
 import qualified System.Exit as Exit
- 
-test1 :: Test
-test1 = TestCase (assertEqual "should return 3" 3 (basicSum 1 2))
- 
+
+testGetMuls :: Test
+testGetMuls = TestList [
+    "One valid" ~: getMuls "mul(1,2)" ~?= ["mul(1,2)"],
+    "Empty string" ~: getMuls "" ~?= [],
+    "Get multiple without space" ~: getMuls "mul(1,3)dumymul(999,1)" ~?= ["mul(1,3)", "mul(999,1)"],
+    "Get multiple with space" ~: getMuls "mul(1,3) dumy mul(999,1)" ~?= ["mul(1,3)", "mul(999,1)"],
+    "mul with zero at first place" ~: getMuls "mul(0,1)" ~?= [],
+    "mul with zero at second place" ~: getMuls "mul(1,0)" ~?= [],
+    "extra char inside" ~: getMuls "mul(1,g1)" ~?= [],
+    "two follow up" ~: getMuls "mul(1,1)mul(999,999)" ~?= ["mul(1,1)", "mul(999,999)"],
+    "multiple valid with spaces" ~: getMuls "mul(1,2) mul(3,4) mul(5,6)" ~?= ["mul(1,2)", "mul(3,4)", "mul(5,6)"],
+    "multiple valid without spaces" ~: getMuls "mul(1,2)mul(3,4)mul(5,6)" ~?= ["mul(1,2)", "mul(3,4)", "mul(5,6)"],
+    "mixed valid and invalid" ~: getMuls "mul(1,2) mul(0,4) mul(5,6)" ~?= ["mul(1,2)", "mul(5,6)"]
+  ]
+
+
+testGetLineMul :: Test
+testGetLineMul = TestList [
+    "One valid" ~: getLineMul "mul(1,2)" ~?= 2,
+    "Zero at first place" ~: getLineMul "mul(0,2)" ~?= 0,
+    "Zero at second place" ~: getLineMul "mul(1,0)" ~?= 0,
+    "Two digits" ~: getLineMul "mul(10,20)" ~?= 200,
+    "Three digits" ~: getLineMul "mul(100,200)" ~?= 20000
+  ]
+
 tests :: Test
-tests = TestList [TestLabel "test1" test1]
- 
+tests = TestList [
+    TestLabel "Test GetMuls" testGetMuls,
+    TestLabel "Test GetLineMul" testGetLineMul
+  ]
+
 main :: IO ()
 main = do
     result <- runTestTT tests
